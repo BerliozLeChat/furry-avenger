2934d4449f7d7c67e01f8bbdcd21a19097808a16
BerliozLeChat <neoflash93@gmail.com>
2014-02-13 14:53:03 +0100

    Creation classe MultiEnsemble

--- src/MultiEns.cpp
+++ src/MultiEns.cpp
@@ -7,11 +7,41 @@
    
    par les créateur de furry-hypster, ils reviennent encore plus fort. #furry-avenger
      
-   
 */
 
 #include "MultiEns.hpp"
-#include <cmath> // utile pour les fonctions mathématiques
+#include <cmath> // utile pour les fonctions mathématique
 #include <sstream> // pour les conversions en chaine
 
+
+
 using namespace std;
+
+class MultiEnsemble{
+		private :
+			
+		
+		public :
+	
+}
+
+	MultiEnsemble::MultiEnsemble(){//constructeur
+	}
+	void MultiEnsemble::ajouter(T elt){//ajoute un élément
+	}	
+	~ MultiEnsemble::oteUn(T elt){//ote l'élément passé en paramètre
+	}
+	~ MultiEnsemble::oteTous(T elt){//Ote tous les éléments elt de l'ensemble
+	}
+	bool MultiEnsemble::estVide(){//Vrai si est vide
+	}
+	int MultiEnsemble::nbOcc(T elt){//Retourne le nombre d'occurence d'elt
+	}
+	void MultiEnsemble::fusionner(MultiEnsemble me){//fusionne deux ensembles
+	}
+	MultiEnsemble MultiEnsemble::intersecte(MultiEnsemble me){//donne l'intersection de deux ensembles
+	}
+	void MultiEnsemble::enleve(MultiEnsemble me){//enlève l'intersection de deux ensembles
+	}
+	bool MultiEnsemble::egal(MultiEnsemble me){//vrai si les ensembles sont équivalents
+	}
--- src/MultiEns.hpp
+++ src/MultiEns.hpp
@@ -9,6 +9,35 @@
 
 #include <string> // pour le type std::string
 
+template <typename T>
+
+class MultiEnsemble{
+
+
+	private :
+		typedef struct _maille _maille;
+		struct _maille{
+			T elt;
+			_maille * suivant;	
+		};
+		
+		_maille * tete;
+		int nbElement;
+
+	public :
+		MultiEnsemble();//constructeur
+		void ajouter(T elt);//ajoute un élément
+		~ oteUn(T elt);//ote l'élément passé en paramètre
+		~ oteTous(T elt);//ote tous les éléments elt de l'ensemble
+		bool estVide();//vrai si est vide
+		int nbOcc(T elt);//retourne le nombre d'occurence d'elt
+		void fusionner(MultiEnsemble me);//fusionne deux ensembles
+		MultiEnsemble intersecte(MultiEnsemble me);//donne l'intersection de deux ensembles
+		void enleve(MultiEnsemble me);//enlève l'intersection de deux ensembles
+		bool egal(MultiEnsemble me);//vrai si les ensembles sont équivalents
+}
+
+
 
 /******************************************************************************/
 
