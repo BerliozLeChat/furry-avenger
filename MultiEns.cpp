5d35d256f30229fcd7face2fd6436cd5cb82a5c1
BerliozLeChat <neoflash93@gmail.com>
2014-02-13 13:59:14 +0100

    ajout des fichiers

---
+++ MultiEns.cpp
@@ -0,0 +1,17 @@
+/* 
+   Fichier MultiEns.cpp
+
+   Définition des méthodes de MultiEns
+   
+   Antoine Boutruche & François Hallereau & Sébastien Vallée
+   
+   par les créateur de furry-hypster, ils reviennent encore plus fort. #furry-avenger
+     
+   
+*/
+
+#include "MultiEns.hpp"
+#include <cmath> // utile pour les fonctions mathématiques
+#include <sstream> // pour les conversions en chaine
+
+using namespace std;
---
+++ MultiEns.hpp
@@ -0,0 +1,15 @@
+/* 
+   Fichier MultiEns.hpp
+
+   Définition du type MultiEns
+*/
+
+#ifndef MULTIENS_HPP
+#define MULTIENS_HPP
+
+#include <string> // pour le type std::string
+
+
+/******************************************************************************/
+
+#endif // MULTIENS_HPP
