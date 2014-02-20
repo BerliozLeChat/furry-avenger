3a3efe81e99d2b5256c86ddcfdd39ec126cce015
Sebastien VALLEE <E115308K@I003V2pc05.ensinfo.sciences.univ-nantes.prive>
2014-02-20 15:17:47 +0100

    correction d'erreur completion de fonctions

--- src/MultiEns.cpp
+++ src/MultiEns.cpp
@@ -9,39 +9,90 @@
      
 */
 
-#include "MultiEns.hpp"
-#include <cmath> // utile pour les fonctions mathématique
+
+ // utile pour les fonctions mathématique
 #include <sstream> // pour les conversions en chaine
 
 
 
 using namespace std;
 
-class MultiEnsemble{
-		private :
-			
+template <typename T>
+	MultiEnsemble<T>::MultiEnsemble(){//constructeur
+		this->tete=NULL;
+		this->nbElement=0;
+	}
+	template <typename T>
+	void MultiEnsemble<T>::ajouter(T elt){//ajoute un élément
+		_maille * maillon;
+		maillon->elt = elt;
+		maillon->suivant=NULL;
+		_maillon * pointeur = this->tete;
+		if(this->nbELement==0){
+			this->tete=maillon;
+		}
+		else{
+			while(pointeur->suivant!=NULL){
+				pointeur=pointeur->suivant;
+			}
+			pointeur->suivant=maillon;
+		}
 		
-		public :
-	
-}
-
-	MultiEnsemble::MultiEnsemble(){//constructeur
-	}
-	void MultiEnsemble::ajouter(T elt){//ajoute un élément
+		this->nbElement=this->nbElement+1;
+			
 	}	
-	~ MultiEnsemble::oteUn(T elt){//ote l'élément passé en paramètre
+	template <typename T>
+	void MultiEnsemble<T>::oteUn(T elt){//ote l'élément passé en paramètre
+		if(this->nbElement!=0){
+				_maille * pointeurPrecedent=NULL;
+				_maille * pointeur=this->tete;
+				bool trouve = false;
+				while(pointeur!=NULL && trouve==false){
+					
+					if(pointeur->elt==elt){
+						if(pointeurPrecedent!=NULL){
+							trouve=true;
+							_maille * tmp=pointeur->suivant;
+							delete(pointeur);
+							pointeurPrecedent->suivant=tmp;
+						}
+						else{
+							this->tete=pointeur->suivant;
+							delete(pointeur);
+						}
+					}
+					pointeurPrecedent=pointeur;
+					pointeur=pointeur->suivant;
+						
+				}
+				
+		}
 	}
-	~ MultiEnsemble::oteTous(T elt){//Ote tous les éléments elt de l'ensemble
+	template <typename T>
+	void MultiEnsemble<T>::oteTous(T elt){//Ote tous les éléments elt de l'ensemble
+		return 0;
 	}
-	bool MultiEnsemble::estVide(){//Vrai si est vide
+	template <typename T>
+	bool MultiEnsemble<T>::estVide(){//Vrai si est vide
+		return 0;
 	}
-	int MultiEnsemble::nbOcc(T elt){//Retourne le nombre d'occurence d'elt
+	template <typename T>
+	int MultiEnsemble<T>::nbOcc(T elt){//Retourne le nombre d'occurence d'elt
+		return 0;
 	}
-	void MultiEnsemble::fusionner(MultiEnsemble me){//fusionne deux ensembles
+	template <typename T>
+	void MultiEnsemble<T>::fusionner(MultiEnsemble me){//fusionne deux ensembles
+			return 0;
 	}
-	MultiEnsemble MultiEnsemble::intersecte(MultiEnsemble me){//donne l'intersection de deux ensembles
+	template <typename T>
+	MultiEnsemble MultiEnsemble<T>::intersecte(MultiEnsemble me){//donne l'intersection de deux ensembles
+			return 0;
 	}
-	void MultiEnsemble::enleve(MultiEnsemble me){//enlève l'intersection de deux ensembles
+	template <typename T>
+	void MultiEnsemble<T>::enleve(MultiEnsemble me){//enlève l'intersection de deux ensembles
+			return 0;
 	}
-	bool MultiEnsemble::egal(MultiEnsemble me){//vrai si les ensembles sont équivalents
+	template <typename T>
+	bool MultiEnsemble<T>::egal(MultiEnsemble me){//vrai si les ensembles sont équivalents
+			return 0;
 	}
--- src/MultiEns.hpp
+++ src/MultiEns.hpp
@@ -10,16 +10,16 @@
 #include <string> // pour le type std::string
 
 template <typename T>
-
 class MultiEnsemble{
 
 
 	private :
-		typedef struct _maille _maille;
+		
 		struct _maille{
 			T elt;
 			_maille * suivant;	
 		};
+		typedef struct _maille _maille;
 		
 		_maille * tete;
 		int nbElement;
@@ -27,18 +27,20 @@ class MultiEnsemble{
 	public :
 		MultiEnsemble();//constructeur
 		void ajouter(T elt);//ajoute un élément
-		~ oteUn(T elt);//ote l'élément passé en paramètre
-		~ oteTous(T elt);//ote tous les éléments elt de l'ensemble
+		void oteUn(T elt);//ote l'élément passé en paramètre
+		void oteTous(T elt);//ote tous les éléments elt de l'ensemble
 		bool estVide();//vrai si est vide
 		int nbOcc(T elt);//retourne le nombre d'occurence d'elt
 		void fusionner(MultiEnsemble me);//fusionne deux ensembles
 		MultiEnsemble intersecte(MultiEnsemble me);//donne l'intersection de deux ensembles
 		void enleve(MultiEnsemble me);//enlève l'intersection de deux ensembles
 		bool egal(MultiEnsemble me);//vrai si les ensembles sont équivalents
-}
+};
 
 
 
 /******************************************************************************/
-
+#include "MultiEns.tpp"
 #endif // MULTIENS_HPP
+
+
